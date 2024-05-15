0449647d99762179736d002a0e102da953ab62e3
Nicholas-Westby <nicholas-westby@users.noreply.github.com>
2024-05-14 21:54:42 -0700

    support object pivots

--- src/loader.ts
+++ src/loader.ts
@@ -1,15 +1,26 @@
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
-import {Scene} from "three";
+import {Group, Object3DEventMap, Scene} from "three";
 
-export async function addObject({scene, name, scale}: {scene: Scene, name: string, scale?: number}) {
+export async function addObject({scene, name, scale}: {scene: Scene, name: string, scale?: number}): Promise<Group<Object3DEventMap>> {
   const loader = new GLTFLoader().setPath(`/assets/models/${name}/`);
-  loader.load(`${name}.gltf`, async (gltf) => {
-    const model = gltf.scene;
+  return new Promise((resolve) => {
+    loader.load(`${name}.gltf`, async (gltf) => {
+      const model = gltf.scene;
 
-    const factor = scale || 0.1;
-    model.scale.set(factor, factor, factor);
-    model.position.y += 1;
+      const factor = scale || 0.1;
+      model.scale.set(factor, factor, factor);
+      model.position.y += 1;
 
-    scene.add(model);
+      // Add a group so we can easily pivot around the origin.
+      const group = new Group();
+      group.position.set(0, 0, 0);
+      group.rotation.set(0, 0, 0);
+      group.add(model);
+      scene.add(group);
+
+      scene.add(group);
+
+      resolve(group);
+    });
   });
 }
