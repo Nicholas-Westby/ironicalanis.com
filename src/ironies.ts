7ca604ef18c1ac98fd139859211a3ad5de18b354
Nicholas-Westby <nicholas-westby@users.noreply.github.com>
2024-05-22 21:10:55 -0700

    simplify data for ironies

--- src/ironies.ts
+++ src/ironies.ts
@@ -1,13 +1,17 @@
 import ironies from "./data/ironies.json";
 
+/**
+ * The data associated with an irony (i.e., the times in which they appear throughout the video,
+ * as well as the name of the 3D model).
+ */
 export type Irony = {
-  name: string;
-  ironic: boolean;
   times: number[];
-  phrase: string;
   model?: string;
 };
 
+/**
+ * Retrieves the list of ironies from the JSON file.
+ */
 export async function getIronies() : Promise<Irony[]> {
   return ironies;
 }
