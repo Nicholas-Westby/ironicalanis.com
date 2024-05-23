e2eb1fb218808a9d18dc171602d8b9ba34cfccd0
Nicholas-Westby <nicholas-westby@users.noreply.github.com>
2024-05-22 21:25:43 -0700

    comment materials file

--- src/material.ts
+++ src/material.ts
@@ -1,5 +1,8 @@
 import {Object3D} from "three";
 
+/**
+ * Ensures all materials in the specified model support transparency.
+ */
 export async function transparentMaterial({model}: {model: Object3D}): Promise<Object3D> {
   model.traverse((x: Object3D) => {
     if (!(x as any).isMesh) {
