2f17d0457cc00e4119e9460d9589f2f477b3e03f
Nicholas-Westby <nicholas-westby@users.noreply.github.com>
2024-05-22 21:17:29 -0700

    comment irony animation file

--- src/irony-animation.ts
+++ src/irony-animation.ts
@@ -5,6 +5,13 @@ const videoEl = document.querySelector('video');
 let lastVideoTime: number,
   lastPerfTime: number;
 
+/**
+ * Retrieves the number of seconds elapsed since the start of the video. Combines playhead time
+ * with elapsed time to ensure a smooth animation experience. This nuanced approach might not be
+ * useful most of the time, but playhead time may be imprecise due to things like timing attacks.
+ *
+ * See: https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/currentTime
+ */
 function getSeconds() {
   const seconds = videoEl!.currentTime,
     now = performance.now();
@@ -16,6 +23,9 @@ function getSeconds() {
   return seconds + diff / 1000.0;
 }
 
+/**
+ * Animates the ironies by updating their rotation based on the current time.
+ */
 export async function animateIronies() {
   const ironies = await getInsertedIronies(),
     seconds = getSeconds(),
@@ -25,11 +35,17 @@ export async function animateIronies() {
     const relevantTime = irony.times.find(time =>
       seconds >= time + start && seconds <= time + end);
     if (relevantTime) {
+
+      // Rotate the irony into view.
       const progress = (seconds - relevantTime) / (end - start);
       const factor = 2.2;
       irony.model.rotation.x = progress * factor;
+
     } else {
+
+      // Keep the irony out of view (behind the sphere).
       irony.model.rotation.x = -2;
+
     }
   }
 }
